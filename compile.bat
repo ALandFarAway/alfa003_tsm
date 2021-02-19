ffbdfe8ee29bb6aa9aeb31e327608cb89f1a5b5d
mgmd144 <mgmd144@gmail.com>
2021-02-19 00:51:08 -0600

    new compile bat for new box

--- compile.bat
+++ compile.bat
@@ -1,3 +1,13 @@
-del *.ncs
-del *.ndb
-NWNScriptCompiler.exe -e -v1.70 -o -h "C:\NWN2User" -m alfa003_tsm .\*.nss -y
+set HOMEDIR="%userprofile%\Documents\Neverwinter Nights 2"
+set NWN2DIR="C:\Program Files (x86)\GOG Galaxy\Games\NWN2 Complete"
+
+call set PARENT_DIR=%CD%
+set PARENT_DIR=%PARENT_DIR:\= %
+set LAST_WORD=
+for %%i in (%PARENT_DIR%) do set LAST_WORD=%%i
+set MODULE=%LAST_WORD%
+
+del .\*.ncs
+del .\*.ndb
+
+NWNScriptCompiler.exe -e -v1.70 -o -h %HOMEDIR% -m %MODULE% -n %NWN2DIR% .\*.nss -y
